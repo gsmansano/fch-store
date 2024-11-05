f4182c6dc0a73bb674be2312f4ed1557e167b15f
Geovane Mansano <ge_mansano@hotmail.com>
2024-11-05 16:33:44 +0000

    started on product pages. (created unit and categories due to requirements)

--- src/Lw.FchStore.Domain/Entities/Product.cs
+++ src/Lw.FchStore.Domain/Entities/Product.cs
@@ -6,7 +6,6 @@ namespace Lw.FchStore.Domain.Entities
     {
         [Key]
         public int ProductId { get; set; }
-
         public int CategoryId { get; set; }
         public int SupplierId { get; set; }
         public int ManufacturerId { get; set; }
